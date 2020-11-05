0c2afcfe7091bb9a14e30dd7497729b3466bd2bb
Your Name <you@example.com>
2020-11-05 15:29:30 +0700

    .

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 require "./config.php";
 $sql = 'SELECT * FROM HangHoa where SoLuongHang>0';
 $result = $conn->query($sql);
-echo $result->fetch_all()[0]['TenHH'];
+$hanghoa = $result->fetch_all(MYSQLI_ASSOC);
+
+require './view/index.php';
--- view/index.php
+++ view/index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Shop</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script src="main.js"></script>
+    <script>
+    </script>
     <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
@@ -93,211 +94,40 @@
                         <img class="category-item-img" src="Capture.PNG" alt="">
                     </div>
                 </div>
-                <div class="col-2 category-col">
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                </div>
-                <div class="col-2 category-col">
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                </div>
-                <div class="col-2 category-col">
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                </div>
-                <div class="col-2 category-col">
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                </div>
-                <div class="col-2 category-col">
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                    <div class="category-item">
-                        <div class="category-item-description">
-                            adsa
-                        </div>
-                        <img class="category-item-img" src="Capture.PNG" alt="">
-                    </div>
-                </div>
-
             </div>
         </div>
-        <div class="products ">
+        <div class="products">
             <div class="row">
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
+                <?php
+                for ($i = 0; $i < count($hanghoa); $i++) {
+                    echo '<div class="col-2">
+                            <a href=".chitiethanghoa/dd" class="products-item">
+                                <img class="products-item-img" src="' . $hanghoa[$i]['Hinh'] . '" alt="">
+                                <p>
+                                ' . $hanghoa[$i]['MoTaHH'] . '
+                                    <!-- 26 chữ -->
+                                </p>
+                                <div>
+                                    <div class="products-item-price">
+                                        ' . $hanghoa[$i]['Gia'] . '
+                                    </div>
+                                </div>
+                            </a>
+                        </div>';
+                }
+                ?>
 
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-2 ">
-                    <a href="#" class="products-item">
-                        <img class="products-item-img" src="picture/Capture.png" alt="">
-                        <p>
-                            QQQQQQQQQQQQQQQQQQQQQQQQQQ
-                            <!-- 26 chữ -->
-                        </p>
-                        <div>
-                            <div class="products-item-price">
-                                50.000đ
-                            </div>
-                            <div>
-
-                            </div>
-                        </div>
-                    </a>
-                </div>
             </div>
-
         </div>
 
-    </div>
-    <div class="footer container">
-    </div>
-    <!-- JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <div class="footer container">
+
+        </div>
+        <!-- JS, Popper.js, and jQuery -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
