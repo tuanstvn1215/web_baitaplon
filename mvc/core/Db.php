38415e6f5c5adeac0326a0bfec2bd9dea9c4d3d1
Your Name <you@example.com>
2020-11-12 23:31:11 +0700

    add DB->update

--- mvc/core/Db.php
+++ mvc/core/Db.php
@@ -9,7 +9,7 @@ class Db
 
     public function __construct()
     {
-
+        //
         $this->conn = new mysqli($this->db_servername, $this->db_username, $this->db_password, $this->db_name);
         $this->conn->set_charset("utf8");
     }
